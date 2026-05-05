9628208ee383fe35fc26dd88d4304dc598b17efb
davilacerdadefreitasouzalc-star <davilacerdadefreitasouzalc@gmail.com>
2026-05-05 09:37:28 -0300

    Add files via upload

---
+++ painel dark.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import time
+senha = input("qual a senha")
+if senha <"20":
+    print("senha incorreta")
+    exit()
+    print("senha correta")   
+    time.sleep(3)
+    os.system("clear")
+else:
+    print("seja bem vindo painel dark client")  
+print("carregando")
+time.sleep(2)
+os.system("clear")
+os.system("sl")
+time.sleep(3)
+print("⠀⠀⠀ ⢀⣤⣶⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀⠀")
+print("⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀")
+print("⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀")
+print("⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀")
+print("⠀⠀⠀⢰⡟⠛⠉⠙⢻⣿⡟⠋⠉⠙⢻⡇⠀⠀⠀⠀")
+print("⠀⠀⠀⢸⣷⣀⣀⣠⣾⠛⣷⣄⣀⣀⣼⡏⠀⠀⠀⠀")
+print(" ⠀⠀⠛⠋⢻⣿⣧⣤⣸⣿⡟⠙⠛⠀⠀⠀⠀")
+print(" ⢀⣰⣿⣦⠀⠼⣿⣿⣿⣿⣿⡷⠀⠀⠀⣰⣿⣆⡀")
+print(" ⢻⣿⣿⣿⣧⣄⠀⠁⠉⠉⠋⠈⠀⠀⣀⣴⣿⣿⣿⡿")
+print("⠀⠀⠀⠈⠙⠻⣿⣶⣄⡀⠀⢀⣠⣴⣿⠿⠛⠉⠁⠀⠀")
+print("⠀⠀⠀⠀⠀⠀⠉⣻⣿⣷⣿⣟⠉⠀⠀⠀⠀⠀⠀⠀")
+print("⠀⠀⠀⢀⣠⣴⣿⠿⠋⠉⠙⠿⣷⣦⣄⡀⠀⠀⠀⠀")
+print("⣴⣶⣶⣾⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣷⣶⣶⣦")
+print("⠙⢻⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⡿⠋")
+
+print("1: spam ")
+print("2: nuke ")
+print("3: raid dark ")
+
+escolha = False
+
